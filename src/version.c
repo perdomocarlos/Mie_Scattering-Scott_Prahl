6c3b177e849857eceae2a4a4ac00cb68f9dfff0e
Carlos Fernandez <animecomico@yahoo.es>
2017-10-05 03:14:17 -0500

    -Mie update version

--- src/version.c
+++ src/version.c
@@ -2,4 +2,4 @@
 /* version number is set in                   <../Makefile>  */
 /* date is the last modification date for     <../Makefile>  */
 
-char *Version = "2-4-1 (08:52:53 08/01/17)";
+char *Version = "2-5-0 (03:11:27 10/05/17)";
